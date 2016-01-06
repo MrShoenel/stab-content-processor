c3e88fb8bdfb853614f398cddcf6e3ffa968f05e
MrShoenel <development@hoenel.net>
2016-01-06 18:24:41 +0100

    Add index-file to export function for creating Html from markdown and content.json

---
+++ index.js
@@ -0,0 +1,8 @@
+var createContentJSON = require('./modules/createContentJson.js'),
+	markdownContent = require('./modules/markdownContent.js');
+
+// Just export both functions
+module.exports = {
+	createContentJSON: createContentJSON,
+	markdownContent: markdownContent
+};
